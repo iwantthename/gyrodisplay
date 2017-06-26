250caca76c27fbfb4bc9a057f9dee321973b7795
ishizaki_shijimi <rsk62682@gmail.com>
2017-06-26 17:41:34 +0900

    ネットからコピペし、コンパイルエラーを修正

--- gyrodisplay/gyrodisplay.cpp
+++ gyrodisplay/gyrodisplay.cpp
@@ -1,11 +1,27 @@
-// gyrodisplay.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
-//
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include "serial.h"
 
-#include "stdafx.h"
-
-
-int main()
+int main(void)
 {
-    return 0;
-}
+	serial_t obj = serial_create("COM1", 9600);
+	unsigned char buf[128], len;
 
+	if (obj == NULL) {
+		fprintf(stderr, "オブジェクト生成に失敗");
+		return EXIT_FAILURE;
+	}
+
+	while (1) {
+		len = serial_recv(obj, buf, sizeof(buf));
+		if (len) serial_send(obj, buf, len);
+		Sleep(1);
+		if (_kbhit())  break;
+	}
+
+	serial_delete(obj);
+
+	return EXIT_SUCCESS;
+}
